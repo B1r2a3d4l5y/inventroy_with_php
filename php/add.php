fc8a53ade9ca68bee057c8d3e2a34ab5eef2681c
Bradley Wayne Arnold <82877822+B1r2a3d4l5y@users.noreply.github.com>
2022-12-14 12:39:59 +0200

    Add files via upload

--- php/add.php
+++ php/add.php
@@ -9,29 +9,30 @@
 
     if(empty($product_name)|| empty($product_price)|| empty($quanity)){
       $emptyError = "Fields are empty. Please fill them in";
-      header("Location:./index.html?inventroy=empty");  
+      header("Location:./index.php?inventroyempty");  
      }  elseif(!is_numeric($product_price)){
         $product_priceError = "Price field must contain numbers only";
-        header("Location:../index.html?inventroy=priceinvalidchar");
+        header("Location:../index.php?inventroypriceinvalidchar");
 
       } elseif(!is_numeric($quanity)){
           $quanityError = "Quanity field must contain numbers only";
-          header("Location:../index.html?inventroy=quanityinvalidchar");
+          header("Location:../index.php?inventroyquanityinvalidchar");
 
 
         } else {
           $sql = "INSERT INTO products(product_name, product_price, quanity) VALUES(?,?,?);";
-         stmt = mysqli_stmt_init($conn);
+         $stmt = mysqli_stmt_init($conn);
          //check sql errors
          if(!mysqli_stmt_prepare($stmt, $sql)) {
           die("SQL error") ;
          } else {
           mysqli_stmt_bind_param($stmt, "sss", $product_name, $product_price, $quanity);
-          mysqli_stmt_excute($stmt);
+          mysqli_stmt_execute($stmt);
          }
           
         }
   }
+  header("Location:../index.php");
   $conn->close();
       
     
